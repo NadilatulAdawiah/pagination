5af0e41f0c8ce1baab0b4164b13a309978a90ff3
Ahmad Afrisal <ahmadrajendraa@gmail.com>
2022-02-01 16:26:09 +0800

    Done

--- index.php
+++ index.php
@@ -1,99 +1,106 @@
- <?php
-//tag php untuk menyiapkan data yg akan ditampilkan ke dalam
-//tabel dibawahnya 
+<?php
 session_start();
-if( !isset($_SESSION["login"]) ){
+
+if (!isset($_SESSION["login"])) {
     header("Location: login.php");
     exit;
-    //maksudnya adalah ketika session login tidak ada alihkan kembali ke halaman login
 }
-require 'functions.php'; //pemanggilan file functions
+
+require 'functions.php';
 
 // pagination
-//konfigurasi
-$jumlahDataPerhalaman = 2;
+// konfigurasi
+$jumlahDataPerHalaman = 2;
 $jumlahData = count(query("SELECT * FROM mahasiswa"));
-$jumlahHalaman = ceil($jumlahData / $jumlahDataPerhalaman);
-//menggunakan operator ternary
-$halamanAktif = (isset($_GET["halaman"]) ) ? $_GET["halaman"] : 1;
-$awalData = ( $jumlahDataPerhalaman * $halamanAktif ) - $jumlahDataPerhalaman;
-$mahasiswa = query("SELECT * FROM mahasiswa LIMIT $awalData, $jumlahDataPerhalaman ");
+$jumlahHalaman = ceil($jumlahData / $jumlahDataPerHalaman);
+$halamanAktif = (isset($_GET["halaman"])) ? $_GET["halaman"] : 1;
+$awalData = ($jumlahDataPerHalaman * $halamanAktif) - $jumlahDataPerHalaman;
 
-// tombol cari di klik
-if(isset ($_POST["cari"])){
-    $mahasiswa = cari($_POST["keyword"]); //maksudnya variabel mahasiswa akan berisi sesuai dengan dari function cari, function cari menampilkan apapun yag diketikkan oleh usernya
- }
+$mahasiswa = query("SELECT * FROM mahasiswa LIMIT $awalData, $jumlahDataPerHalaman");
+
+// tombol cari ditekan
+if (isset($_POST["cari"])) {
+    $mahasiswa = cari($_POST["keyword"]);
+}
+
 ?>
-
+<!DOCTYPE html>
 <html>
-    <head>
-        <title>Halaman Admin</title>
-    </head>
-    <body>
-    
-    <a href="logout.php"> Logout</a>
+
+<head>
+    <title>Halaman Admin</title>
+</head>
+
+<body>
+
+    <a href="logout.php">Logout</a>
+
     <h1>Daftar Mahasiswa</h1>
-    <a href="tambah.php">Tambah data mahasiswa</a> 
-    <br></br>
+
+    <a href="tambah.php">Tambah data mahasiswa</a>
+    <br><br>
 
     <form action="" method="post">
-        <input type="text" name="keyword" size="40" autofocus placeholder="Masukkan keyword pencarian..."
-        autocomplete ="off">
-        <button type="submit" name="cari"> Cari!</button>
-        
-        <!-- autofocus adalah tag untuk membuat menu serachhing auto aktif saat masuk halaman
-            placeholder adalah tag untuk memasukkan kata pada button 
-            autocomplete untuk menghilangkan histori pencarian user-->
+
+        <input type="text" name="keyword" size="40" autofocus placeholder="masukkan keyword pencarian.." autocomplete="off">
+        <button type="submit" name="cari">Cari!</button>
+
     </form>
+    <br><br>
 
     <!-- navigasi -->
-    <?php if( $halamanAktif > 1) : ?>
-        <a href="?halamam=<?= $halamanAktif - 1; ?>">&laquo;</a>
+    <a href="?halaman=1">awal</a>
+
+    <?php if ($halamanAktif > 1) : ?>
+        <a href="?halaman=<?= $halamanAktif - 1; ?>">&laquo;</a>
     <?php endif; ?>
 
-    <?php for($i = 1; $i <= $jumlahHalaman; $i++ ) : ?>
-        <?php if( $i == $halamanAktif ) : ?>
+    <?php for ($i = 1; $i <= $jumlahHalaman; $i++) : ?>
+        <?php if ($i == $halamanAktif) : ?>
             <a href="?halaman=<?= $i; ?>" style="font-weight: bold; color: red;"><?= $i; ?></a>
-        <?php else: ?>
+        <?php else : ?>
             <a href="?halaman=<?= $i; ?>"><?= $i; ?></a>
         <?php endif; ?>
     <?php endfor; ?>
 
-    <?php if( $halamanAktif < $jumlahHalaman) : ?>
-        <a href="?halamam=<?= $halamanAktif + 1; ?>">&raquo;</a>
+    <?php if ($halamanAktif < $jumlahHalaman) : ?>
+        <a href="?halaman=<?= $halamanAktif + 1; ?>">&raquo;</a>
     <?php endif; ?>
 
+    <a href="?halaman=<?= $jumlahHalaman; ?>">akhir</a>
 
-    <br><br>
+    <br>
     <table border="1" cellpadding="10" cellspacing="0">
-     
-    <tr>
-        <th>No. </th>
-        <th>Aksi </th>
-        <th>Gambar </th>
-        <th>NIM </th>
-        <th>Nama</th>
-        <th>Jurusan</th>
-        <th>Email</th>
-    </tr>
-    <?php $i = 1; ?>
-    <?php foreach ($mahasiswa as $row ) : ?>
-    <tr>
-        <td> <?= $i; ?> </td>
-        <td>
-            <a href="ubah.php?id=<?= $row["id"] ?>"> ubah </a> |
-            <a href="hapus.php?id=<?= $row["id"]; ?>" 
-                onclick="return confirm('Apakah anda yakin ingin menghapus data?');"> hapus </a> 
-        </td>
-        <td><img src="image/<?= $row["gambar"]; ?>"
-        width="40"</td>
-        <td> <?= $row["nim"]; ?> </td>
-        <td> <?= $row["nama"]; ?></td>
-        <td> <?= $row["jurusan"]; ?></td>
-        <td> <?= $row["email"]; ?></td>
-    </tr>
-    <?php $i++ ?>
-    <?php endforeach; ?>
 
-    </body>
+        <tr>
+            <th>No.</th>
+            <th>Aksi</th>
+            <th>Gambar</th>
+            <th>NRP</th>
+            <th>Nama</th>
+            <th>Email</th>
+            <th>Jurusan</th>
+        </tr>
+
+        <?php $i = 1; ?>
+        <?php foreach ($mahasiswa as $row) : ?>
+            <tr>
+                <td><?= $i; ?></td>
+                <td>
+                    <a href="ubah.php?id=<?= $row["id"]; ?>">ubah</a> |
+                    <a href="hapus.php?id=<?= $row["id"]; ?>" onclick="return confirm('yakin?');">hapus</a>
+                </td>
+                <td><img src="image/<?= $row["gambar"]; ?>" width="50"></td>
+                <td><?= $row["nim"]; ?></td>
+                <td><?= $row["nama"]; ?></td>
+                <td><?= $row["email"]; ?></td>
+                <td><?= $row["jurusan"]; ?></td>
+            </tr>
+            <?php $i++; ?>
+        <?php endforeach; ?>
+
+    </table>
+
+</body>
+
 </html>
